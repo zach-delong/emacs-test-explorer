ff073d67e39df7bab5150242f436aa18ee056c5c
Zachery DeLong <zacherypd@gmail.com>
2024-04-07 14:36:45 -0400

    Writing tests for functions

--- tests/create-entry.el
+++ tests/create-entry.el
@@ -1,9 +1,13 @@
 (require 'emacs-test-explorer)
 
-(ert-deftest create-explorer-entry ()
+(ert-deftest create-explorer-entry-status ()
   "testing if create explorer entry works properly"
   (should (equal 'Y (car (create-explorer-entry 'Y "Test Description"))))
-  (should (equal 'N (car (create-explorer-entry 'N "Test Description"))))
-  (should (equal "Description" (car (nthcdr 1 (create-explorer-entry 'Y "Description"))))))
+  (should (equal 'N (car (create-explorer-entry 'N "Test Description")))))
+
+(ert-deftest crate-explorer-entry-description ()
+  "Ensure the description loads correctly"
+  (should (equal "Description" (car (nthcdr 1 (create-explorer-entry 'Y "Description")))))
+  (should (equal () (car (nthcdr 1 (create-explorer-entry 'Y ()))))))
 
 (provide 'create-entry)

7ac07d137e66743f35901ec2bd72b59ff27828a8
Zachery DeLong <zacherypd@gmail.com>
2024-04-07 14:33:28 -0400

    Checked in first new unit test

--- tests/create-entry.el
+++ tests/create-entry.el
@@ -3,6 +3,7 @@
 (ert-deftest create-explorer-entry ()
   "testing if create explorer entry works properly"
   (should (equal 'Y (car (create-explorer-entry 'Y "Test Description"))))
-  (should (equal 'N (car (create-explorer-entry 'N "Test Description")))))
+  (should (equal 'N (car (create-explorer-entry 'N "Test Description"))))
+  (should (equal "Description" (car (nthcdr 1 (create-explorer-entry 'Y "Description"))))))
 
 (provide 'create-entry)
